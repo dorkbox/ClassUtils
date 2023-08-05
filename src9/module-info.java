6b3982b72f7e703dabb916ee0f9a0478afa4777f
Robinson <robinson@dorkbox.com>
2023-08-05 12:07:36 -0600

    Added jpms modules info

---
+++ src9/module-info.java
@@ -0,0 +1,8 @@
+module dorkbox.classUtils {
+    exports dorkbox.classUtil;
+
+    requires transitive dorkbox.collections;
+    requires transitive net.jodah.typetools;
+
+    requires transitive kotlin.stdlib;
+}
